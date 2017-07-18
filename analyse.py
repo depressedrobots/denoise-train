1625fb7c3eb7a1b2dc5990c298d85ca17b5517f7
Wojciech A. Jurczyk <wajurczyk@gmail.com>
2017-07-18 21:57:59 +0200

    Threshold on luma channel diff.

--- analyse.py
+++ analyse.py
@@ -14,22 +14,36 @@ if False == os.path.exists(path):
 if len(filenames) == 0:
     sys.exit("no files in /frames")
 
-lastImgGrayInt = None
+diffValues = np.zeros(256)
+
+thresh = 50
+lastImgYUV = None
+lastImgYUVInt = None
 for i, filename in enumerate(filenames):
     print('processing ' + str(i+1) + '/' + str(len(filenames)))
 
     filepath = path + '/' + filename
     currImg = cv2.imread(filepath)
-    currImgGray = cv2.cvtColor(currImg, cv2.COLOR_BGR2GRAY)
-    currImgGrayInt = currImgGray.astype('int')
+    currImgYUV = cv2.cvtColor(currImg, cv2.COLOR_BGR2YUV_I420)
 
-    # special case: idx 0
+    # special case: idx 0, can't compare with previous frame
     if i != 0:
-       diff = currImgGrayInt - lastImgGrayInt
-       diff = abs(diff)
-       diffUInt = diff.astype('uint8')
-       newFilename = 'output/' + format(i, '06') + '.png'
-       cv2.imwrite(newFilename, diffUInt)
+        currImgYUVInt = currImgYUV.astype('int')
+        diff = currImgYUVInt.copy()
+        diff[:479] = currImgYUVInt[:479] - lastImgYUVInt[:479]
+        diff = abs(diff)
+
+        #for x in np.nditer(diff):
+         #   diffValues[x] += 1
+
+        # where diff < thresh, overwrite new pixel with former pixel 
+        currImgYUV[diff<thresh] = lastImgYUV[diff<thresh]
+
+        recentImgBGR = cv2.cvtColor(currImgYUV, cv2.COLOR_YUV2BGR_I420)
+        newFilename = './output/' + format(i, '06') + '.png'
+        cv2.imwrite(newFilename, recentImgBGR)
     
-    lastImgGrayInt = currImgGrayInt
-    
+    lastImgYUV = currImgYUV
+    lastImgYUVInt = lastImgYUV.astype('int')    
+
+#np.savetxt('diffvalues.txt', diffValues)
